31f4703ff0a447c0bfa86328305dd8d4ccc546d6
Carter Ball <carter.k.ball@gmail.com>
2022-08-06 15:52:02 -0400

    Work on shifts method, need to refactor Offset

--- spec/shift_spec.rb
+++ spec/shift_spec.rb
@@ -4,23 +4,24 @@ require './shift'
 
 describe Shift do
   before :each do
-    @shift = Shift.new("02715", "040895")
+    @shift = Shift.new(Key.new("021715"), Offset.new)
   end
 
-  xit 'exists' do
+  it 'exists' do
     expect(@shift).to be_a(Shift)
   end
 
   it 'has a key' do
-    expect(@shift.key).to eq("02715")
+    expect(@shift.key).to be_a(Key)
   end
 
-  it 'has a date to generate offests' do
-    expect(@shift.date).to eq("040895")
+  it 'has an offset' do
+    expect(@shift.offset).to be_a(Offset)
   end
 
-  xit 'can make a shifts hash with the right values' do
+  it 'can make a shifts hash with the right values' do
     expect(@shift.shifts).to be_a(Hash)
+    require 'pry';binding.pry
     expect(@shift.shifts).to eq({A => 3, B=> 27, C => 73, D => 20} )
   end
 
