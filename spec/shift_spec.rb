ce4188aafb2ff4eb09efcb9f6da29d3305fed740
Carter Ball <carter.k.ball@gmail.com>
2022-08-06 15:14:37 -0400

    Add additional tests to Shift

--- spec/shift_spec.rb
+++ spec/shift_spec.rb
@@ -1,19 +1,28 @@
-# require './key'
-# require './offset'
-# require './shift'
+require './key'
+require './offset'
+require './shift'
 
-# describe Shift do
-#   before :each do
-#     @shift = Shift.new
-#   end
+describe Shift do
+  before :each do
+    @shift = Shift.new("02715", "040895")
+  end
 
-#   xit 'exists' do
-#     expect(@shift).to be_a(Shift)
-#   end
+  xit 'exists' do
+    expect(@shift).to be_a(Shift)
+  end
 
-#   xit 'can make a shifts hash with the right values' do
-#     expect(@shift.shifts).to eq(A => )
-#   end
+  it 'has a key' do
+    expect(@shift.key).to eq("02715")
+  end
+
+  it 'has a date to generate offests' do
+    expect(@shift.date).to eq("040895")
+  end
+
+  xit 'can make a shifts hash with the right values' do
+    expect(@shift.shifts).to be_a(Hash)
+    expect(@shift.shifts).to eq({A => 3, B=> 27, C => 73, D => 20} )
+  end
 
 
 
@@ -25,4 +34,4 @@
 
 
 
-# end
+end
