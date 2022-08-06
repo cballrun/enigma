cc394c0c2438c0aad85de3167aaa4704339ba757
Carter Ball <carter.k.ball@gmail.com>
2022-08-05 20:37:11 -0400

    Add optional argument to Key class for testing, new instantiation of key in tests, tests for A, B, C, D keys

--- lib/key.rb
+++ lib/key.rb
@@ -1,11 +1,14 @@
 class Key
+  
+  attr_reader :test_key
 
-  def initialize
-
+  def initialize(test_key = nil)
+    @test_key = test_key
   end
 
   def keygen
     number = rand(99999).to_s
+    number.rjust(5, "0")
   end
 
 
--- spec/key_spec.rb
+++ spec/key_spec.rb
@@ -2,17 +2,43 @@ require './key'
 
 describe Key do
   before :each do
-    @key = Key.new
+    @key = Key.new("02715")
+    @key_1 = Key.new
   end
 
   it 'exists' do
     expect(@key).to be_a(Key)
+    expect(@key_1).to be_a(Key)
+  end
+
+  it 'can initialize with a predefined test key' do
+    expect(@key.test_key).to eq("02715")
   end
 
   it 'can create a random 5 digit number as a string' do #.format for leading zeroes/string padding? or ljust/rjust?
     expect(@key.keygen.length).to eq(5)
     expect(@key.keygen.class).to eq(String)
+    expect(@key_1.keygen.length).to eq(5)
+    expect(@key_1.keygen.class).to eq(String)
   end
+
+  xit 'has an a key' do
+    expect(@key.a_key).to eq(02)
+  end
+
+  xit 'has an b key' do
+    expect(@key.b_key).to eq(27)
+  end
+
+  xit 'has an c key' do
+    expect(@key.c_key).to eq(71)
+  end
+
+  xit 'has an d key' do
+    expect(@key.d_key).to eq(15)
+  end
+
+
 
 
 
