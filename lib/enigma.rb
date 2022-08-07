fb585593a9da380213b77f9deb7936e579cfa058
Carter Ball <carter.k.ball@gmail.com>
2022-08-07 16:21:25 -0400

    Begin working on encrypt, need to refactor offset square method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,18 @@
 class Enigma
+  
 
+  def initialize
 
+  end
 
+  def encrypt(message, key = nil, date = nil)
+    key = Key.new(key)
+    offset = Offset.new(date)
+    require 'pry';binding.pry
+    shift = Shift.new(key, offset) 
+    encryption = Encryption.new(message, shift)
+    encryption_hash = Hash.new
+    encryption_hash[:encryption] = encryption.encrypt_message
+    
+  end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,8 +1,18 @@
 require './enigma'
+require './shift'
+require './key'
+require './encryption'
+require './decryption'
 
 describe Enigma do
   before :each do
-    
+    @key = Key.new("02715")
+    @date = Timecop.freeze(1995, 8, 4)
+    @offset = Offset.new(@date)
+    require 'pry';binding.pry
+    @shift = Shift.new(@key, @offset)
+    @encryption = Encryption.new("hello world", @shift)
+    @decryption = Decryption.new("keder ohulw", @shift)
     @enigma = Enigma.new
   end
 
@@ -10,7 +20,9 @@ describe Enigma do
     expect(@enigma).to be_a(Enigma)
   end
 
-  xit 'can encrypt a message with a key and date' do
+
+  it 'can encrypt a message with a key and date' do
+    
     expect(@enigma.encrypt("hello world", "02715", "040895")).to eq(
       {
            
@@ -64,8 +76,8 @@ describe Enigma do
     expect(encrypted).to eq(
     {
            
-      encryption: #"keder ohulw",
-      key: #"02715",
+      encryption: "keder ohulw",
+      key: "02715",
       date: "070822"
          
     }
