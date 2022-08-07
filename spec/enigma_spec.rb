c790c45a982c033999080889507d72a3e8e79495
Carter Ball <carter.k.ball@gmail.com>
2022-08-07 15:29:35 -0400

    Tests for Enigma class set up, final test not yet finished

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -2,6 +2,7 @@ require './enigma'
 
 describe Enigma do
   before :each do
+    
     @enigma = Enigma.new
   end
 
@@ -9,6 +10,68 @@ describe Enigma do
     expect(@enigma).to be_a(Enigma)
   end
 
+  xit 'can encrypt a message with a key and date' do
+    expect(@enigma.encrypt("hello world", "02715", "040895")).to eq(
+      {
+           
+        encryption: "keder ohulw",
+        key: "02715",
+        date: "040895"
+         
+      })
+  end
+
+  xit 'can decrypt a message with a key and date' do
+    expect(@enigma.decrypt("keder ohulw", "02715", "040895")).to eq(
+    {
+
+     decryption: "hello world",
+     key: "02715",
+     date: "040895"
+
+    })
+  end
+
+  xit 'can encrypt a message with a key and todays date' do
+    encrypted = @enigma.encrypt("hello world", "02715")
+    expect(encrypted).to eq(
+    {
+           
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "070822"
+         
+    }
+    )
+  end
+
+  xit 'can decrypt a message with a key and todays date' do
+    encrypted = enigma.encrypt("hello world", "02715")
+    expect(@enigma.decrypt(encrypted[:encryption], "02715")).to eq(
+      {
+  
+       decryption: "hello world",
+       key: "02715",
+       date: "070822"
+  
+      })
+
+
+  end
+
+  xit 'can encrypt a message with a random key and todays date' do #not done
+    encrypted = @enigma.encrypt("hello world")
+    expect(encrypted).to eq(
+    {
+           
+      encryption: #"keder ohulw",
+      key: #"02715",
+      date: "070822"
+         
+    }
+    )
+  end
 
 
 end
+
