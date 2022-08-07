e252eb45b637b2a32b1dbb20279b6b93f787b65f
Carter Ball <carter.k.ball@gmail.com>
2022-08-07 03:37:37 -0400

    Remove unused methods and tests from Encryption

--- spec/encryption_spec.rb
+++ spec/encryption_spec.rb
@@ -27,7 +27,6 @@ describe Encryption do
     expect(@encryption.alphabet). to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
   end
 
-
   it 'can index the alphabet' do
     expect(@encryption.alphabet_with_index).to eq({"a" => 0,
       "b" => 1,
@@ -59,65 +58,32 @@ describe Encryption do
   end
 
   it 'can encrypt a one letter message with the a shift' do
-    
     expect(@encryption.encrypt_letter_a("h")).to eq("k")
   end
   
   it 'can encrypt a one letter message with the b shift' do
-    
     expect(@encryption.encrypt_letter_b("e")).to eq("e")
   end
 
   it 'can encrypt a one letter message with the c shift' do
-    
     expect(@encryption.encrypt_letter_c("l")).to eq("d")
   end
 
   it 'can encrypt a one letter message with the d shift' do
-   
-    
-    expect(@encryption.encrypt_letter_d("l")).to eq("e")
+   expect(@encryption.encrypt_letter_d("l")).to eq("e")
   end
-
 
   it 'can split a message into an array of characters' do
     expect(@encryption.message_split).to eq(["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"])
   end
-
-  
 
   it 'can split a message array into sub arrays' do
     expect(@encryption.chars_split).to be_a(Array)
     expect(@encryption.chars_split).to eq([["h", "e", "l", "l"], ["o", " ", "w", "o"], ["r", "l", "d"]])
   end
 
-  xit 'can enrypt the characters in the sub arraay' do
-    
-    expect(@encryption.chars_encrypt).to eq([["k", "e", "d", "e"], ["r", " ", "o", "h"], ["u", "l", "w"]])
-  end
-
-  xit 'can index the sub arrays 0..3' do
-    
-    expect(@encryption.chars_index).to be_a(Hash)
-    expect(@encryption.chars_index).to eq({0=>["h", "o", "r"], 1=>["e", " ", "l"], 2=>["l", "w", "d"], 3=>["l", "o"]})
-  end
-
-
-  
-  xit 'can encrypt the characters of a message' do
-    expect(@encryption.encrypt_characters).to be_a(Array)
-    expect(@encryption.encrypt_characters).to eq(["k", "r", "o", "e", "w", "d", "d", "g", "e", "k"])
-  end
 
   it 'can encrypt a message' do
-    
     expect(@encryption.encrypt_message).to eq("keder ohulw")
   end
-
-
-
-
-
-
-
 end
