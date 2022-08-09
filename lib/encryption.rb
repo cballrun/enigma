d6049c32e353bfd0526f1b8187f93bcf4ddf41e2
Carter Ball <carter.k.ball@gmail.com>
2022-08-08 21:28:37 -0400

    Update encrypt_message and decrypt_message to deal with capitals and chars not in alphabet_with_index

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -45,14 +45,16 @@ class Encryption
     encrypted_message = ""
     chars_split.map do|chars|
       chars.each_with_index do |char, index|
-        if index == 0
-          encrypted_message << encrypt_letter_a(char)
+        if !alphabet_with_index.include?(char.downcase)
+          encrypted_message << char
+        elsif index == 0
+          encrypted_message << encrypt_letter_a(char.downcase)
         elsif index == 1
-          encrypted_message << encrypt_letter_b(char)
+          encrypted_message << encrypt_letter_b(char.downcase)
         elsif index == 2
-          encrypted_message << encrypt_letter_c(char)
+          encrypted_message << encrypt_letter_c(char.downcase)
         elsif index == 3
-          encrypted_message << encrypt_letter_d(char)
+          encrypted_message << encrypt_letter_d(char.downcase)
         end
       end
     end
