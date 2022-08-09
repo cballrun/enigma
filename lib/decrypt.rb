5b28436f496891d44d05d974398150f84c20c4bc
Carter Ball <carter.k.ball@gmail.com>
2022-08-08 20:21:40 -0400

    Some work on Decrypt, returning to Encrypt

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,9 +1,18 @@
 require './lib/enigma'
+require './lib/enigma'
+require './lib/key'
+require './lib/offset'
+require './lib/shift'
+require './lib/decryption'
 
-class Decryption
-
-
-
-
+class Decrypt
   
+  # message = File.open("decrypted.txt").read
+  # enigma = Enigma.new
+  # decrypted_message = enigma.decrypt(message, #need key, #need date)
+  
+  # File.write(("decrypted.txt"), decrypted_message[:decryption])
+
+  # puts "Created encrypted.txt with the key #{encrypted_message[:key]} and the date #{encrypted_message[:date]}"
+
 end
